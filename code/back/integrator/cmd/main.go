2437d04221858f91b5e26f8e30603207da288052
2gury <egor3359@bk.ru>
2024-11-26 20:47:41 +0300

    Add LLM generated Hello World programm

--- code/back/integrator/cmd/main.go
+++ code/back/integrator/cmd/main.go
@@ -1,4 +1,51 @@
+// main.go
+
 package main
 
+import (
+	"context"
+	"encoding/json"
+	"net/http"
+
+	"github.com/go-kit/kit/endpoint"
+	gokithttp "github.com/go-kit/kit/transport/http"
+	"log"
+)
+
+// Define the request and response structures
+type helloRequest struct{}
+
+type helloResponse struct {
+	Message string `json:"message"`
+}
+
+// Create the endpoint
+func makeHelloEndpoint() endpoint.Endpoint {
+	return func(ctx context.Context, request interface{}) (interface{}, error) {
+		return helloResponse{Message: "Hello World from Go Kit!"}, nil
+	}
+}
+
+// Decode the request (no parameters needed)
+func decodeHelloRequest(_ context.Context, _ *http.Request) (interface{}, error) {
+	return helloRequest{}, nil
+}
+
+// Encode the response as JSON
+func encodeResponse(_ context.Context, w http.ResponseWriter, response interface{}) error {
+	return json.NewEncoder(w).Encode(response)
+}
+
 func main() {
+	// Create a handler for the endpoint
+	helloHandler := gokithttp.NewServer(
+		makeHelloEndpoint(),
+		decodeHelloRequest,
+		encodeResponse,
+	)
+
+	// Start the HTTP server
+	http.Handle("/hello", helloHandler)
+	log.Println("Server is running on port 8080...")
+	log.Fatal(http.ListenAndServe(":8080", nil))
 }
--- code/back/integrator/go.mod
+++ code/back/integrator/go.mod
@@ -1,3 +1,10 @@
 module github.com/2Delight/mline/code/integrator
 
 go 1.23.0
+
+require github.com/go-kit/kit v0.13.0
+
+require (
+	github.com/go-kit/log v0.2.0 // indirect
+	github.com/go-logfmt/logfmt v0.5.1 // indirect
+)
